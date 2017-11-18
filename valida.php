07d88c9d7c276e0202472f9668a94a88fac60ee0
jagjunior <jagjunior23@gmail.com>
2017-11-18 00:20:40 -0500

    se suben archivos nuevamente porque se presentaban problemas en archivos dentro de github

---
+++ valida.php
@@ -0,0 +1,33 @@
+<?php 
+include("conectar.php"); 
+
+$correo = $_POST["email"]; 
+$password = md5($_POST["password"]);
+
+$con = conex();
+
+$consulta = consulta_sql($con,"SELECT mail,password FROM users WHERE mail='$correo' AND password ='$password'");
+
+$row = mysqli_fetch_array($consulta, MYSQLI_NUM);
+
+if($correo=="" || $password=="")
+{
+	$error="Usuario o Contrasena Incorrecta";
+        echo $error;
+}else{
+	if($row[0]!="" && $row[1]!="")
+	{
+		if($row[0] == $correo && $row[1] == $password)
+		{
+			session_start();
+			$_SESSION["mail"]= $correo;
+			$_SESSION['start'] = time();
+			$_SESSION['expire'] = $_SESSION['start'] + (50 * 60);
+			$ok="ok";
+			echo $ok;
+		}
+	}else{
+		echo $error;
+		}
+	}
+?>
